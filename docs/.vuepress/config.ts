d82d75f2c44d8a3a158415ed488be8b8c9fb3393
MeiChen <13316899740@163.com>
2023-06-21 23:50:09 +0800

    编写markdown语法

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -33,7 +33,7 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
     nav: [
       
       { text: '首页', link: '/' },
-      { text: '目录导航栏', link: '/pages/8211f8/' },
+      { text: '导航栏', link: '/pages/8211f8/' },
       {
         text: '运维',
         link: '/Operation_maintenance/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
